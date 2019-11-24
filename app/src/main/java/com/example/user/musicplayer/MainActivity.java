abd4ddd4846444b4a02c7e9c28733bc1d97db402
Yxc0816 <626170462@qq.com>
2019-11-24 11:07:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/musicplayer/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.user.musicplayer;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.user.musicplayer.adapter.*;
+import com.example.user.musicplayer.fragment.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+//实现OnClickListener的接口
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView logicTv;
+    private TextView onlineTv;
+    private ViewPager viewPager;
+    private ImageView menuImagv;
+    private ImageView seachImagv;
+    //将Fragment放入List集合中，存放fragment对象
+    private List<Fragment> fragmentList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //绑定id
+        bindID();
+        //设置监听
+        listen();
+        //创建fragment对象
+        LogicFragment logicFragment = new LogicFragment();
+        //将fragment对象添加到fragmentList中
+        fragmentList.add(logicFragment);
+        //通过MusicPagerAdapter类创建musicPagerAdapter的适配器，下面我将添加MusicPagerAdapter类的创建方法
+        MusicPagerAdapter musicPagerAdapter = new MusicPagerAdapter(getSupportFragmentManager(), fragmentList);
+        //viewPager绑定适配器
+        viewPager.setAdapter(musicPagerAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            //页面切换，改变标题的样式
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        logicTv.setTextColor(getResources().getColor(R.color.white));
+                        break;
+                    case 1:
+                        logicTv.setTextColor(getResources().getColor(R.color.white_60P));
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+    }
+
+    private void listen() {
+        logicTv.setOnClickListener(this);
+       // menuImagv.setOnClickListener(this);
+    }
+
+    private void bindID() {
+        logicTv = findViewById(R.id.main_logic_tv);
+        viewPager = findViewById(R.id.main_vp);
+       // menuImagv = findViewById(R.id.main_menu_imgv);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_logic_tv:
+                //实现点击TextView切换fragment
+                viewPager.setCurrentItem(0);
+                break;
+           // case R.id.main_menu_imgv:
+               // break;
+            default:
+                break;
+        }
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_HOME) {
+            final View view = LayoutInflater.from(this).inflate(R.layout.dialog1_layout, null);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("退出提示");
+            builder.setView(view);
+            builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(MainActivity.this, "确定退出播放器吗", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+            builder.show();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
+
---
+++ app/src/main/java/com/example/user/musicplayer/utils/Common.java
@@ -0,0 +1,15 @@
+package com.example.user.musicplayer.utils;
+
+import com.example.user.musicplayer.entity.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shaoyangyang on 2017/12/20.
+ */
+
+public class Common {
+
+    public static List<Music> musicList = new ArrayList<>();
+}
